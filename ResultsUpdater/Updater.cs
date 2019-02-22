028dd06b30de6b4e3ec061f0f0aed9697f42fc3d
alfleo23 <alonsoleone1@tiscali.it>
2019-02-22 16:20:52 +0000

    links now open in a new tab
    Signed-off-by: alfleo23 <alonsoleone1@tiscali.it>

--- ResultsUpdater/Updater.cs
+++ ResultsUpdater/Updater.cs
@@ -24,49 +24,53 @@ namespace ResultsUpdater
                 //get the saved searches
                 var savedSearches = _context.SavedSearch.OrderByDescending(x => x.CreatedDate).Include(x => x.Results).ToList();
 
-                foreach (var search in savedSearches)
+                if (savedSearches.Count != 0)
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"scraping prices for search: {search.Description} , id {search.SavedSearchId}, created on: {search.CreatedDate}");
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    
-                    Hashtable amazonResults;
-                    Hashtable ebayResults;
-                    Hashtable jLewisResults;
-
-                    try
+                    foreach (var search in savedSearches)
                     {
-                        jLewisResults = await jLewis.ScrapePricesForProduct(search.Description);
-                        ebayResults = await ebay.ScrapePricesForProduct(search.Description);
-                        amazonResults = await amazon.ScrapePricesForProduct(search.Description);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"scraping prices for search: {search.Description} , id {search.SavedSearchId}, created on: {search.CreatedDate}");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                
+                        Hashtable amazonResults;
+                        Hashtable ebayResults;
+                        Hashtable jLewisResults;
+
+                        try
+                        {
+                            jLewisResults = await jLewis.ScrapePricesForProduct(search.Description);
+                            ebayResults = await ebay.ScrapePricesForProduct(search.Description);
+                            amazonResults = await amazon.ScrapePricesForProduct(search.Description);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                            throw;
+                        }
+
+                        var result = new Result()
+                        {
+                            Date = DateTime.Now,
+                            AmazonPrice = Convert.ToDouble(amazonResults["Formatted Price"]),
+                            EbayPrice = Convert.ToDouble(ebayResults["Formatted Price"]),
+                            JohnLewisPrice = Convert.ToDouble(jLewisResults["Formatted Price"]),
+                            AmazonLink = amazonResults["Product Link"].ToString(),
+                            AmazonHeading = amazonResults["Product Heading"].ToString(),
+                            EbayLink = ebayResults["Product Link"].ToString(),
+                            EbayHeading = ebayResults["Product Heading"].ToString(),
+                            JohnLewisHeading = jLewisResults["Product Heading"].ToString(),
+                            JohnLewisLink = jLewisResults["Product Link"].ToString(),
+                        };
+
+                        search.Results.Add(result);
+                        await _context.SaveChangesAsync();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("completed ");
+                        Console.ForegroundColor = ConsoleColor.Gray;
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e);
-                        throw;
-                    }
-
-                    var result = new Result()
-                    {
-                        Date = DateTime.Now,
-                        AmazonPrice = Convert.ToDouble(amazonResults["Formatted Price"]),
-                        EbayPrice = Convert.ToDouble(ebayResults["Formatted Price"]),
-                        JohnLewisPrice = Convert.ToDouble(jLewisResults["Formatted Price"]),
-                        AmazonLink = amazonResults["Product Link"].ToString(),
-                        AmazonHeading = amazonResults["Product Heading"].ToString(),
-                        EbayLink = ebayResults["Product Link"].ToString(),
-                        EbayHeading = ebayResults["Product Heading"].ToString(),
-                        JohnLewisHeading = jLewisResults["Product Heading"].ToString(),
-                        JohnLewisLink = jLewisResults["Product Link"].ToString(),
-                    };
-
-                    search.Results.Add(result);
-                    await _context.SaveChangesAsync();
-
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("completed ");
-                    Console.ForegroundColor = ConsoleColor.Gray;
                 }
+                Console.WriteLine("No Saved Searches to update");
             }
             
             Console.WriteLine($"Update Task completed successfully at {DateTime.Now}");
